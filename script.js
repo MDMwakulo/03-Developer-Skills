c10b146c3f8cb6505885a86c4cf588f786473436
MDMwakulo <mdmwakulo@gmail.com>
2023-05-30 12:39:50 -0700

    Debugging with the Console and Breakpoints

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-// PROBLEM 1
+/*// PROBLEM 1
 // We work for a company building a smart home thermometer. Our
 // most recent task is this: "Given an aray of temperatures of one day, calculate the temperature amplitude.
 // Keep in mind that sometimes there might be a sensor error."
@@ -82,4 +82,56 @@ const mergeTwoArr = (arr1, arr2) => arr1.concat(arr2);
 
 const mergedArr = mergeTwoArr(temp4, temp5);
 console.log(mergedArr);
-console.log(calcTempAmplitude(mergedArr));
+console.log(calcTempAmplitude(mergedArr));*/
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+
+    // C) FIX
+    value: Number(prompt('Degrees celsius:')),
+  };
+
+  // B) FIND
+  console.table(measurement);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) IDENTIFY
+console.log(measureKelvin());
+
+const temp4 = [3, -2, -6, -1, 'error', 9],
+  temp5 = [13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitudeBug = function (temps) {
+  let min, max;
+
+  for (let i = 0; i < temps.length; i++) {
+    if (typeof temps[i] !== 'number') {
+      continue;
+    } else {
+      min = temps[i];
+      max = temps[i];
+      break;
+    }
+  }
+
+  for (let i = 0; i < temps.length; i++) {
+    if (typeof temps[i] !== 'number') {
+      continue;
+    } else {
+      min = Math.min(min, temps[i]);
+      max = Math.max(max, temps[i]);
+    }
+  }
+  console.log({ maxVal: max, minVal: min });
+  let tempAmp = max - min;
+  return tempAmp ? tempAmp : 'error';
+};
+
+const mergeTwoArr = (arr1, arr2) => arr1.concat(arr2);
+
+const mergedArrBug = mergeTwoArr(temp4, temp5);
+console.log(mergedArrBug);
+console.log(calcTempAmplitudeBug(mergedArrBug));
